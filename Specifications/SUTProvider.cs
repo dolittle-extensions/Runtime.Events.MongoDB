cd87eeeec70d60f6ceec2109628a1ffcb8d9ab31
Michael Smith <michael@dolittle.com>
2018-07-22 20:42:46 +0200

    Fix spec to correctly mock out the IApplicationArtifactIdentifierStringConverter

--- Specifications/SUTProvider.cs
+++ Specifications/SUTProvider.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Dolittle.Runtime.Events.Store.Specs;
 using Dolittle.Applications;
 using Moq;
@@ -10,14 +11,22 @@ namespace Dolittle.Runtime.Events.Store.MongoDB.Specs
         private Mock<IApplicationArtifactIdentifierStringConverter> _mock_converter; 
         public SUTProvider()
         {
-            var artifact = Dolittle.Runtime.Events.Store.Specs.given.an_event_store.event_source_artifact;
             _mock_converter = new Mock<IApplicationArtifactIdentifierStringConverter>();
-            _mock_converter.Setup(c => c.FromString(Moq.It.IsAny<string>())).Returns(artifact);
-            _mock_converter.Setup(c => c.AsString(Moq.It.IsAny<IApplicationArtifactIdentifier>())).Returns(Guid.NewGuid().ToString());
+            _mock_converter.Setup(c => c.FromString(Moq.It.IsAny<string>())).Returns((string id) => GetArtifact(id));
+            _mock_converter.Setup(c => c.AsString(Moq.It.IsAny<IApplicationArtifactIdentifier>()))
+                                .Returns((IApplicationArtifactIdentifier aaid) => aaid.GetHashCode().ToString());
             _mock_converter.Setup(c => c.Equals(Moq.It.IsAny<IApplicationArtifactIdentifier>()))
                                 .Returns((IApplicationArtifactIdentifier aaid) => ReferenceEquals(_mock_converter.Object,aaid));
         }
 
         public IEventStore Build() => new test_mongodb_event_store(new a_mongo_db(),_mock_converter.Object);
+
+        static IApplicationArtifactIdentifier GetArtifact(string id)
+        {
+            if(id == Dolittle.Runtime.Events.Store.Specs.given.an_event_store.event_source_artifact.GetHashCode().ToString())
+                return Dolittle.Runtime.Events.Store.Specs.given.an_event_store.event_source_artifact;
+            
+            return Dolittle.Runtime.Events.Store.Specs.given.an_event_store.event_artifacts.Values.SingleOrDefault(v => v.GetHashCode().ToString() == id);
+        }
     }
 }
