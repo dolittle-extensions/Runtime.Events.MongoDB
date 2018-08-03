75c757b9327524ff1e7a36f7c4647e49f6684d9b
Michael Smith <michael@dolittle.com>
2018-08-03 11:11:14 +0200

    More xml comment fixes

--- Source/DateTimeExtensions.cs
+++ Source/DateTimeExtensions.cs
@@ -3,9 +3,16 @@ using System.Linq;
 
 namespace EventStore.MongoDB
 {
-
+    /// <summary>
+    /// Extends DateTime
+    /// </summary>
     public static class DateTimeExtensions 
     {
+        /// <summary>
+        /// Converts a nullable DateTime into its DateTimeOffset equivalent
+        /// </summary>
+        /// <param name="date">The DateTime to convert</param>
+        /// <returns>A DateTimeOffset equivalent if it has a value, otherwise DateTimeOffset.MinValue</returns>
         public static DateTimeOffset ToDateTimeOffset(this DateTime? date)
         {
             return date.HasValue ? date.Value : DateTimeOffset.MinValue;
--- Source/UncommittedEventStreamExtensions.cs
+++ Source/UncommittedEventStreamExtensions.cs
@@ -11,11 +11,11 @@ namespace Dolittle.Runtime.Events.Store.MongoDB
     {
 
         /// <summary>
-        /// Constructs a <see cref="CommittedEventSteam" /> from the <see cref="UncomittedEventSteam" /> and the <see cref="CommitSequenceNumber" /> 
+        /// Constructs a <see cref="CommittedEventStream" /> from the <see cref="UncommittedEventStream" /> and the <see cref="CommitSequenceNumber" /> 
         /// </summary>
-        /// <param name="uncommitted">the <see cref="UncomittedEventSteam" /></param>
+        /// <param name="uncommitted">the <see cref="UncommittedEventStream" /></param>
         /// <param name="sequenceNumber">the <see cref="CommitSequenceNumber" /></param>
-        /// <returns>The corresponding <see cref="CommittedEventSteam" /></returns>
+        /// <returns>The corresponding <see cref="CommittedEventStream" /></returns>
         public static CommittedEventStream ToCommitted(this UncommittedEventStream uncommitted, CommitSequenceNumber sequenceNumber)
         {
             return new CommittedEventStream(sequenceNumber, uncommitted.Source, uncommitted.Id, uncommitted.CorrelationId, uncommitted.Timestamp, uncommitted.Events);
