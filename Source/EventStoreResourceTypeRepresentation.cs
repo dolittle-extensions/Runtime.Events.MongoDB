2166f029acb78d09ef263f86a238c31a0f5284a7
Sindre Wilting <sindrewilting@gmail.com>
2018-10-19 10:06:08 +0200

    Better, faster and stronger

--- Source/EventStoreResourceTypeRepresentation.cs
+++ Source/EventStoreResourceTypeRepresentation.cs
@@ -12,7 +12,12 @@ namespace Dolittle.Runtime.Events.MongoDB
     /// <inheritdoc/>
     public class EventStoreResourceTypeRepresentation : IRepresentAResourceType
     {
-        IDictionary<Type, Type> _bindings;
+        static IDictionary<Type, Type> _bindings = new Dictionary<Type, Type>
+        {
+            {typeof(Dolittle.Runtime.Events.Store.IEventStore), typeof(Dolittle.Runtime.Events.Store.MongoDB.EventStore)},
+            {typeof(Dolittle.Runtime.Events.Relativity.IGeodesics), typeof(Dolittle.Runtime.Events.Relativity.MongoDB.Geodesics)},
+            {typeof(Dolittle.Runtime.Events.Processing.IEventProcessorOffsetRepository), typeof(Dolittle.Runtime.Events.Processing.MongoDB.EventProcessorOffsetRepository)}
+        };
         
         /// <inheritdoc/>
         public ResourceType Type => "eventStore";
@@ -21,22 +26,6 @@ namespace Dolittle.Runtime.Events.MongoDB
         /// <inheritdoc/>
         public Type ConfigurationObjectType => typeof(EventStoreConfiguration);
         /// <inheritdoc/>
-        public IDictionary<Type, Type> Bindings {
-            get 
-            {
-                if (_bindings == null) 
-                    InitializeBindings();
-                
-                return _bindings;
-           }
-        }
-
-        void InitializeBindings()
-        {
-            _bindings = new Dictionary<Type, Type>();
-            _bindings.Add(typeof(Dolittle.Runtime.Events.Store.IEventStore), typeof(Dolittle.Runtime.Events.Store.MongoDB.EventStore));
-            _bindings.Add(typeof(Dolittle.Runtime.Events.Relativity.IGeodesics), typeof(Dolittle.Runtime.Events.Relativity.MongoDB.Geodesics));
-            _bindings.Add(typeof(Dolittle.Runtime.Events.Processing.IEventProcessorOffsetRepository), typeof(Dolittle.Runtime.Events.Processing.MongoDB.EventProcessorOffsetRepository));
-        }
+        public IDictionary<Type, Type> Bindings => _bindings;
     }
 }
