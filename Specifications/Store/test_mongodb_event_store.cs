0096bb659ad422fb9a6e17fff0e190e2200a6c81
Michael Smith <michael@dolittle.com>
2018-11-28 13:18:08 +0100

    Remove versions, just query the commit table

--- Specifications/Store/test_mongodb_event_store.cs
+++ Specifications/Store/test_mongodb_event_store.cs
@@ -21,11 +21,7 @@ namespace Dolittle.Runtime.Events.Store.MongoDB.Specs
         {
             _database_runner = database_runner;
         }
-
-        public void TestUpdateVersion(VersionedEventSource version)
-        {
-            this.UpdateVersion(version);
-        }
+        
         protected override void Dispose(bool disposing)
         {
             if (!disposedValue)
