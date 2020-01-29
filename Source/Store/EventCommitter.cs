649559ad606d21fca4080746e7842b6e2bb6a0f5
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-01-29 19:36:50 +0100

    Commit events for aggregates

---
+++ Source/Store/EventCommitter.cs
@@ -0,0 +1,149 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Dolittle.Applications;
+using Dolittle.Artifacts;
+using Dolittle.Execution;
+using Dolittle.Runtime.Events.Store.MongoDB.EventLog;
+using Dolittle.Tenancy;
+using MongoDB.Driver;
+
+namespace Dolittle.Runtime.Events.Store.MongoDB
+{
+    /// <summary>
+    /// A class capable of committing events.
+    /// </summary>
+    public class EventCommitter
+    {
+        readonly IClientSessionHandle _transaction;
+        readonly IMongoCollection<Event> _eventLog;
+        readonly Cause _cause;
+        readonly CorrelationId _correlation;
+        readonly Microservice _microservice;
+        readonly TenantId _tenant;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EventCommitter"/> class.
+        /// </summary>
+        /// <param name="transaction">The <see cref="IClientSessionHandle"/> representing the MongoDB transaction encompassing this aggregate root version update.</param>
+        /// <param name="eventLog">The <see cref="IMongoCollection{Event}"/> where events are stored in the event store.</param>
+        /// <param name="cause">The <see cref="Cause"/> of the events.</param>
+        /// <param name="correlation">The <see cref="CorrelationId"/> of the events.</param>
+        /// <param name="microservice">The <see cref="Microservice"/> that produced the events.</param>
+        /// <param name="tenant">The <see cref="TenantId"/> the events were produced in.</param>
+        public EventCommitter(IClientSessionHandle transaction, IMongoCollection<Event> eventLog, Cause cause, CorrelationId correlation, Microservice microservice, TenantId tenant)
+        {
+            _transaction = transaction;
+            _eventLog = eventLog;
+            _cause = cause;
+            _correlation = correlation;
+            _microservice = microservice;
+            _tenant = tenant;
+        }
+
+        /// <summary>
+        /// Commits a single <see cref="UncommittedEvent"/> to the event log.
+        /// </summary>
+        /// <param name="version">The expected next <see cref="EventLogVersion"/> of the event log.</param>
+        /// <param name="occured">The <see cref="DateTimeOffset"/> when the event occured.</param>
+        /// <param name="event">The <see cref="UncommittedEvent"/> to commit.</param>
+        /// <param name="committedEvent">The <see cref="CommittedEvent"/> that has been written to the event log.</param>
+        /// <returns>A value indicating whether the commit operation was successful.</returns>
+        public bool TryCommitEvent(EventLogVersion version, DateTimeOffset occured, UncommittedEvent @event, out CommittedEvent committedEvent)
+        {
+            if (InsertEvent(version, occured, @event, new AggregateMetadata()))
+            {
+                committedEvent = new CommittedEvent(version, occured, _correlation, _microservice, _tenant, _cause, @event.Type, @event.Content);
+                return true;
+            }
+            else
+            {
+                committedEvent = null;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Commits a single <see cref="UncommittedEvent"/> applied to an event source by an aggregate root to the event log.
+        /// </summary>
+        /// <param name="eventSource">The <see cref="EventSourceId"/> the event was applied to.</param>
+        /// <param name="aggregateRoot">The <see cref="Artifact"/> identifying the type of the aggregate root that applied the event.</param>
+        /// <param name="aggregateRootVersion">The <see cref="AggregateRootVersion"/> of the aggregate root that applied the event.</param>
+        /// <param name="version">The expected next <see cref="EventLogVersion"/> of the event log.</param>
+        /// <param name="occured">The <see cref="DateTimeOffset"/> when the event occured.</param>
+        /// <param name="event">The <see cref="UncommittedEvent"/> to commit.</param>
+        /// <param name="committedEvent">The <see cref="CommittedAggregateEvent"/> that has been written to the event log.</param>
+        /// <returns>A value indicating whether the commit operation was successful.</returns>
+        public bool CommitAggregateEvent(EventSourceId eventSource, Artifact aggregateRoot, AggregateRootVersion aggregateRootVersion, EventLogVersion version, DateTimeOffset occured, UncommittedEvent @event, out CommittedAggregateEvent committedEvent)
+        {
+            if (InsertEvent(version, occured, @event, new AggregateMetadata
+            {
+                WasAppliedByAggregate = true,
+                EventSourceId = eventSource,
+                TypeId = aggregateRoot.Id,
+                TypeGeneration = aggregateRoot.Generation,
+                Version = aggregateRootVersion,
+            }))
+            {
+                committedEvent = new CommittedAggregateEvent(eventSource, aggregateRoot, aggregateRootVersion, version, occured, _correlation, _microservice, _tenant, _cause, @event.Type, @event.Content);
+                return true;
+            }
+            else
+            {
+                committedEvent = null;
+                return false;
+            }
+        }
+
+        bool InsertEvent(EventLogVersion version, DateTimeOffset occured, UncommittedEvent @event, AggregateMetadata aggregate)
+        {
+            try
+            {
+                _eventLog.InsertOne(_transaction, new Event
+                {
+                    EventLogVersion = version,
+                    Metadata = new EventMetadata
+                    {
+                        Occured = occured,
+                        Correlation = _correlation,
+                        Microservice = _microservice,
+                        Tenant = _tenant,
+                        CauseType = _cause.Type,
+                        CausePosition = _cause.Position,
+                        TypeId = @event.Type.Id,
+                        TypeGeneration = @event.Type.Generation,
+                    },
+                    Aggregate = aggregate,
+                    Content = @event.Content,
+                });
+                return true;
+            }
+            catch (MongoDuplicateKeyException)
+            {
+                return false;
+            }
+            catch (MongoWriteException exception)
+            {
+                if (exception.WriteError.Category == ServerErrorCategory.DuplicateKey)
+                {
+                    return false;
+                }
+
+                throw;
+            }
+            catch (MongoBulkWriteException exception)
+            {
+                foreach (var error in exception.WriteErrors)
+                {
+                    if (error.Category == ServerErrorCategory.DuplicateKey)
+                    {
+                        return false;
+                    }
+                }
+
+                throw;
+            }
+        }
+    }
+}
--- Source/Store/EventStore.cs
+++ Source/Store/EventStore.cs
@@ -2,9 +2,12 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Dolittle.Artifacts;
 using Dolittle.Logging;
 using Dolittle.Runtime.Events.Store.MongoDB.Aggregates;
+using Dolittle.Runtime.Events.Store.MongoDB.EventLog;
 using MongoDB.Driver;
 
 #pragma warning disable DL0008
@@ -42,10 +45,28 @@ namespace Dolittle.Runtime.Events.Store.MongoDB
             try
             {
                 using var session = _connection.MongoClient.StartSession();
-                return session.WithTransaction((transaction, cancel) =>
+                return session.WithTransaction(
+                (transaction, cancel) =>
                 {
-                    // Write much events
-                    var nextVersion = events.ExpectedAggregateRootVersion + 2;
+                    var eventLogVersion = (uint)_connection.EventLog.CountDocuments(transaction, Builders<Event>.Filter.Empty);
+                    var aggregateRootVersion = events.ExpectedAggregateRootVersion.Value;
+
+                    var committedEvents = new List<CommittedAggregateEvent>();
+                    var eventCommitter = new EventCommitter(transaction, _connection.EventLog, new Cause(CauseType.Command, 0), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid());
+
+                    foreach (var @event in events)
+                    {
+                        if (eventCommitter.CommitAggregateEvent(events.EventSource, events.AggregateRoot, aggregateRootVersion, eventLogVersion, DateTimeOffset.Now, @event, out var committedEvent))
+                        {
+                            committedEvents.Add(committedEvent);
+                            eventLogVersion++;
+                            aggregateRootVersion++;
+                        }
+                        else
+                        {
+                            throw new EventLogDuplicateKeyError(eventLogVersion);
+                        }
+                    }
 
                     var committer = new AggregateVersionCommitter(
                         transaction,
@@ -54,15 +75,15 @@ namespace Dolittle.Runtime.Events.Store.MongoDB
                         events.AggregateRoot.Id,
                         events.ExpectedAggregateRootVersion);
 
-                    if (committer.TryIncrementVersionTo(nextVersion))
+                    if (committer.TryIncrementVersionTo(aggregateRootVersion))
                     {
-                        return new CommittedAggregateEvents(events.EventSource, events.AggregateRoot, events.ExpectedAggregateRootVersion, Array.Empty<CommittedAggregateEvent>());
+                        return new CommittedAggregateEvents(events.EventSource, events.AggregateRoot, events.ExpectedAggregateRootVersion, committedEvents);
                     }
                     else
                     {
                         throw new AggregateRootConcurrencyConflict(0, 0);
                     }
-                });
+                }, new TransactionOptions(maxCommitTime: TimeSpan.FromSeconds(5)));
             }
             catch (AggregateRootConcurrencyConflict)
             {
